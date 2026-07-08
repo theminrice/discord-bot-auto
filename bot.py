7e91e4d537443acbab5d9e09a938b9e813b7d9a4
theminrice <bambangwahyuono.rpl2@gmail.com>
2026-07-08 22:40:59 +0700

    Fix message formatting in bot.py

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ from datetime import datetime
 # ==================== CONFIGURATION ====================
 TOKEN_AKUN = "TOKEN_REDACTED "
 CHANNEL_ID = "733050809314705458"
-PESAN = ""SELL
+PESAN = """SELL
 SURG-E 3 :WL~6:  
 LEGAL BRIEFS 180:WL~6: 
-SELL AT :Arrow~4:  ORUHC""
+SELL AT :Arrow~4:  ORUHC"""
 # =======================================================
 
 def kirim_pesan():
